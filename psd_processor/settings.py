56762a824478383b8bb8913a93f23b69fd56c465
AshishPython11 <ashishpython11@gmail.com>
2025-06-19 12:55:16 +0530

    Set DEBUG to False for production readiness

--- psd_processor/settings.py
+++ psd_processor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7#hj=nxj*57xk16s7-hglss^^71g=@-*bq)auo8dc9(zszmx=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
