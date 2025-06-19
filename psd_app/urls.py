2bda0a5417e36d4485dd2a678d51add2465bf804
AshishPython11 <ashishpython11@gmail.com>
2025-06-19 13:01:55 +0530

    Update URL pattern for PSD upload endpoint

--- psd_app/urls.py
+++ psd_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import upload_psd  # Make sure this import exists
 
 urlpatterns = [
-    path('', upload_psd, name='psd_upload'),  # This defines the URL name
+    path('psd_processor/', upload_psd, name='psd_upload'),  # This defines the URL name
 ]
